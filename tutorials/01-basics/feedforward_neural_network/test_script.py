ca6c307ee11a9dafb6d47008482468097b3068ba
Amir Jevnisek <ajenvisek@gmail.com>
2022-09-24 12:19:39 +0300

    Add test script.

---
+++ tutorials/01-basics/feedforward_neural_network/test_script.py
@@ -0,0 +1,73 @@
+import torch
+import torch.nn as nn
+import torchvision
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Hyper-parameters
+input_size = 784
+hidden_size = 500
+num_classes = 10
+num_epochs = 5
+batch_size = 100
+learning_rate = 0.001
+
+# MNIST dataset
+train_dataset = torchvision.datasets.MNIST(root='../../data',
+                                           train=True,
+                                           transform=transforms.ToTensor(),
+                                           download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='../../data',
+                                          train=False,
+                                          transform=transforms.ToTensor())
+
+# Data loader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
+
+
+# Fully connected neural network with one hidden layer
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+
+
+def plot_images(X, y, yp, M, N):
+    f, ax = plt.subplots(M, N, sharex=True, sharey=True, figsize=(N, M*1.3))
+    for i in range(M):
+        for j in range(N):
+            ax[i][j].imshow(1-X[i*N+j][0].cpu().numpy(), cmap="gray")
+            title = ax[i][j].set_title("Pred: {}".format(yp[i*N+j].max(dim=0)[1]))
+            plt.setp(title, color=('g' if yp[i*N+j].max(dim=0)[1] == y[i*N+j] else 'r'))
+            ax[i][j].set_axis_off()
+    plt.tight_layout()
+
+
+model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+
+images, labels = next(iter(test_loader))
+images = images.reshape(-1, 28*28).to(device)
+labels = labels.to(device)
+outputs = model(images)
+_, predicted = torch.max(outputs.data, 1)
+plot_image(images, labels, predicted, 3, 6)
+plt.savefig('test_prediction.png')
